678a4c33e4c42b4213d93a8449526281a3ced4e1
kreid06 <37462199+kreid06@users.noreply.github.com>
2019-10-03 11:15:23 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const db = require("../db");
-const bcrypt = require("bcrypt");
-const expressSession = require("express-session");
+const bcrypt = require("bcryptjs");
 
-const sessionOptions = {
-  secret: "i3rlejofdiaug;lsad",
-  resave: false,
-  saveUninitialized: false
-};
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
