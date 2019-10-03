a96a090ce5c83a6afad6af9b723360270a5a2e30
Kevin Reid <kevinreid3light@gmail.com>
2019-10-03 10:08:06 -0400

    setting up db.js and configing route files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const expressSession = require("express-session");
 
+const sessionOptions = {
+  secret: "i3rlejofdiaug;lsad",
+  resave: false,
+  saveUninitialized: false
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
