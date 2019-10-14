0d25868ddf76b23124513e76f87366b19db4bd6a
Kevin Reid <kevinreid3light@gmail.com>
2019-10-14 10:36:31 -0400

    run build command

---
+++ public/precache-manifest.58ec7506c659c212484da5359579336b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552014b52ded33816b7342d06348199d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e19dd535b556ef923e26",
+    "url": "/static/css/main.39d9acb2.chunk.css"
+  },
+  {
+    "revision": "15562bb40009da0039a1",
+    "url": "/static/js/2.fe256ed7.chunk.js"
+  },
+  {
+    "revision": "e19dd535b556ef923e26",
+    "url": "/static/js/main.b6770032.chunk.js"
+  },
+  {
+    "revision": "85fd7e5019f1c61b01e0",
+    "url": "/static/js/runtime-main.4c86fdf6.js"
+  },
+  {
+    "revision": "b9acc81245682759cabcc8cc683e3a31",
+    "url": "/static/media/recycle-hero.b9acc812.jpg"
+  }
+]);
