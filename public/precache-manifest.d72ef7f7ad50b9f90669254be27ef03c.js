82936661f707de17847c650bc54168c2371c05ef
Kevin Reid <kevinreid3light@gmail.com>
2019-10-14 11:16:58 -0400

    front end changes

---
+++ public/precache-manifest.d72ef7f7ad50b9f90669254be27ef03c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3fb5aa13e06d21bcbb7b7c78b0c4d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7468fcf48796f5d972fc",
+    "url": "/static/css/main.39d9acb2.chunk.css"
+  },
+  {
+    "revision": "15562bb40009da0039a1",
+    "url": "/static/js/2.fe256ed7.chunk.js"
+  },
+  {
+    "revision": "7468fcf48796f5d972fc",
+    "url": "/static/js/main.81c710f0.chunk.js"
+  },
+  {
+    "revision": "85fd7e5019f1c61b01e0",
+    "url": "/static/js/runtime-main.4c86fdf6.js"
+  },
+  {
+    "revision": "b9acc81245682759cabcc8cc683e3a31",
+    "url": "/static/media/recycle-hero.b9acc812.jpg"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58e619100c8b134c534b343a57fee4a5.js"
+  "/precache-manifest.d72ef7f7ad50b9f90669254be27ef03c.js"
 );
 
 self.addEventListener('message', (event) => {
